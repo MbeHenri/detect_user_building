76e993c9936a4b1a6fac7628cbbb00fe6ba36413
MbeHenri <henri.mbe@facsciences-uy1.cm>
2024-02-20 19:33:36 +0100

    update commande for collecting

--- collect.py
+++ collect.py
@@ -1,11 +1,25 @@
 import sys
 from src.load import load_signal_pc
 import os
+import json
 
 
-def createdir(path_dir):
-    if not os.path.exists(path_dir):
-        os.makedirs(path_dir)
+def createdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def load2json(path) -> dict:
+    if os.path.exists(path):
+        with open(path, "r") as f:
+            extensions_images = json.load(f)
+        return extensions_images
+    return {}
+
+
+def save2json(obj, path_file):
+    with open(path_file, "w") as fichier_json:
+        json.dump(obj, fichier_json)
 
 
 def main(args):
@@ -13,10 +27,20 @@ def main(args):
         position = str(args[0])
 
         createdir("./datas/compute")
+        controller = load2json("./datas/compute/controller.json")
+
+        try:
+            nbexec = controller[position] + 1
+        except Exception:
+            nbexec = 1
+
         signals = load_signal_pc()
-        with open("./datas/compute/position.csv", "a") as f:
+        with open(f"./datas/compute/datei_{position}.csv", "a") as f:
             for bssid, signal in signals.items():
-                f.write(f"{bssid};{signal};{position}\n")
+                f.write(f"{bssid};{signal};{nbexec}\n")
+
+        controller[position] = nbexec
+        save2json(controller, "./datas/compute/controller.json")
 
     else:
         print("[USAGE]")
